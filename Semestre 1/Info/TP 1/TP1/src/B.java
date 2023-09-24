440ba3a6ad0d86b2284face8b66e2b7947399117
Kykero <129735572+Kykero@users.noreply.github.com>
2023-09-24 22:12:13 +0200

    Delete Semestre 1/Info directory

--- Semestre 1/Info/TP 1/TP1/src/B.java
+++
@@ -1,8 +0,0 @@
-public class B {
-	
-	public static void main(String[] args) {
-		int x = 4;
-		System.out.println("La valeur de x est : " + x);
-		System.out.println("La valeur de 2x est : " + 2*x);
-	}//main
-}//class
